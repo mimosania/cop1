37b42c839d6812e67c352b08b37b06553299e654
anttilehton <anlehton24@students.oamk.fi>
2026-03-31 14:27:08 +0300

    Added dash

--- game/character_body_2d.gd
+++ game/character_body_2d.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 300
 const JUMP_VELOCITY = -400.0
 
+const DASH_SPEED = 900.0
+const DASH_TIME = 0.15
+var dashing = false
+var dash_timer = 0.0
+var facing = 1
 
 func _physics_process(delta: float) -> void:
+	
+	
+	if dashing:
+		dash_timer -= delta
+	if dash_timer <=0:
+		dashing = false
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -13,13 +24,31 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if  direction != 0:
+		facing = direction
+		
+
+		
+	if Input.is_action_just_pressed("dash") and not dashing:
+		dashing = true
+		dash_timer = DASH_TIME
+		velocity.y = 0
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if dashing:
+		velocity.x = facing *DASH_SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if direction:
+			if dashing:
+				velocity.x = direction * DASH_SPEED
+			else:
+				velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()

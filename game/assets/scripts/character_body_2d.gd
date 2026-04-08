803e22d48c11182fbe06a0e144ba80f48e6dd84b
anttilehton <anlehton24@students.oamk.fi>
2026-04-08 21:17:40 +0300

    started own level and combined doublejump and dash

--- game/assets/scripts/character_body_2d.gd
+++ game/assets/scripts/character_body_2d.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300
 const JUMP_VELOCITY = -400.0
+var has_double_jumped
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -21,10 +22,16 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if is_on_floor():
+		has_double_jumped = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("jump") and !is_on_floor() and !has_double_jumped:
+		velocity.y = JUMP_VELOCITY
+		has_double_jumped = true
 		
 	var direction := Input.get_axis("move_left", "move_right")
 	
@@ -42,6 +49,12 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
 	if dashing:
 		velocity.x = direction *DASH_SPEED
 	else:
@@ -52,5 +65,16 @@ func _physics_process(delta: float) -> void:
 				velocity.x = direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+			
+#Animation
+	if has_double_jumped == true:
+		animated_sprite.play("doublejump")
+	if velocity.x == 0 and is_on_floor():
+		animated_sprite.play("idle")
+	if velocity.x != 0 and is_on_floor():
+		animated_sprite.play("walking")
+	
+	
 
 	move_and_slide()

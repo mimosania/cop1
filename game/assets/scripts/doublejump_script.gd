27e34ddf888c1a60d3221f2956827a3e5273293b
eeskola22 <eeskola22@students.oamk.fi>
2026-04-13 19:20:07 +0300

    Added working killzone, integrated it into the map. Added dying mechanics and animations.

--- game/assets/scripts/doublejump_script.gd
+++ game/assets/scripts/doublejump_script.gd
@@ -4,6 +4,12 @@ extends CharacterBody2D
 const SPEED = 300
 const JUMP_VELOCITY = -400.0
 var has_double_jumped
+var alive = true
+func die():
+	animated_sprite.play("död")
+	alive = false
+	velocity.y = JUMP_VELOCITY
+	velocity.x = velocity.x*-1
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -12,36 +18,36 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if alive:
+		if is_on_floor():
+			has_double_jumped = false
+			
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif Input.is_action_just_pressed("jump") and !is_on_floor() and !has_double_jumped:
+			velocity.y = JUMP_VELOCITY
+			has_double_jumped = true
+			
+		var direction := Input.get_axis("move_left", "move_right")
 		
-	if is_on_floor():
-		has_double_jumped = false
-		
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	elif Input.is_action_just_pressed("jump") and !is_on_floor() and !has_double_jumped:
-		velocity.y = JUMP_VELOCITY
-		has_double_jumped = true
-		
-	var direction := Input.get_axis("move_left", "move_right")
-	
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
+		if direction > 0:
+			animated_sprite.flip_h = false
+		elif direction < 0:
+			animated_sprite.flip_h = true
 
 
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	#Animation
-	if has_double_jumped == true:
-		animated_sprite.play("doublejump")
-	if velocity.x == 0 and is_on_floor():
-		animated_sprite.play("idle")
-	if velocity.x != 0 and is_on_floor():
-		animated_sprite.play("walking")
+		#Animation
+		if has_double_jumped == true:
+			animated_sprite.play("doublejump")
+		if velocity.x == 0 and is_on_floor():
+			animated_sprite.play("idle")
+		if velocity.x != 0 and is_on_floor():
+			animated_sprite.play("walking")
 	
 	move_and_slide()

b962f61252d598d86081e19f40aa114d0584d285
eeskola22 <eeskola22@students.oamk.fi>
2026-04-27 17:48:24 +0300

    Fixings

---
+++ game/assets/scripts/goal_flag2.gd
@@ -0,0 +1,9 @@
+extends Area2D
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(body) -> void:
+	timer.start()
+	body.chill()
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://assets/scenes/level_transitio_3.tscn")

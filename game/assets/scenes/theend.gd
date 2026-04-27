308a9edd003e2a431ef5065b3d2296e16dbde19f
mimosania <tuomikoski.senni@gmail.com>
2026-04-27 15:50:06 +0300

    Made the end screen go to the starting screen.

--- game/assets/scenes/theend.gd
+++ game/assets/scenes/theend.gd
@@ -1,9 +1,8 @@
-extends Area2D
+extends Control
 @onready var timer: Timer = $Timer
 
-func _on_body_entered(body) -> void:
+func _ready():
 	timer.start()
-	body.chill()
 
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://assets/scenes/control.tscn")
